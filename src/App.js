630f845cefc90842d5b9ec7bdaa7f56154a1cb33
My-First project <susmagar012@gmail.com>
2023-12-22 08:36:23 +0545

    I finished my todolist Project.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [value, setValue] = useState('');
+  const [todos, setTodos] = useState([]);
+
+  const handleChange = (e) => {
+    const newValue = e.target.value;
+    setValue(newValue);
+  };
+
+  const addTodo = () => {
+    // Check if 'value' is not empty
+    if (value.trim() !== '') {
+      // Update the 'todos' state with the new todo
+      setTodos((prevTodos) => [...prevTodos, value]);
+
+      // Reset the 'value' state
+      setValue('');
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Container">
+      <h1 className='heading'>To do List</h1>
+      <div className='input-container'>
+        <input type='text' value={value} onChange={handleChange} />
+        <button onClick={addTodo}>Add Todo</button>
+        <ol>
+          {todos.map((todo, index) => {
+            return <li key={index}>{todo}</li>;
+          })}
+        </ol>
+      </div>
     </div>
   );
 }
